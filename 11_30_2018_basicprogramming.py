18323f1b953a7b06aa3aca2be555bc56e9e9da69
ndas950 <45467927+ndas950@users.noreply.github.com>
2018-11-30 11:32:40 -0500

    Add files via upload

---
+++ 11_30_2018_basicprogramming.py
@@ -0,0 +1,55 @@
+#11/30/2018 basic programming
+
+
+
+print("7+5 =",7+5)
+x=25
+greeting="hello!"
+firstName="Nishad"
+lastName="Das"
+
+print(greeting,firstName,lastName)
+print(greeting,firstName + " " + lastName)
+
+
+
+def main():
+    print("This is the computer science intro class")
+    print("Thank you for using this program!")
+
+
+main()
+#main()
+#main()
+
+def question1():
+    #put your code over here
+    print("Something")
+
+def question2():
+    print("This is question 2")
+
+def chaosProgram():
+    #print, eval, input, range, are reserved words for Python
+    # for is a coding structure
+    print("This program illustrates a chaotic function!")
+    x=eval(input("Enter a number between 0 and 1:"))
+    for count in range(10):
+        x=3.9*x*(1-x)
+        print(x)
+    
+    print("Thank you for using this program!")
+
+
+
+
+
+
+
+
+
+
+
+
+           
+
