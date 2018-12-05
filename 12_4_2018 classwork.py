21e7a516544efd187cd66606a1d1e71e6b393ac2
ndas950 <45467927+ndas950@users.noreply.github.com>
2018-12-05 12:41:15 -0500

    Add files via upload

---
+++ 12_4_2018 classwork.py
@@ -0,0 +1,24 @@
+#12/4/2018 for loop
+
+
+def loopDemo():
+
+    for count in range(10):#count is called the loop control variable
+        print("Hello",count)
+        print(2*count)
+        
+    print(count)
+#loopDemo()
+
+
+def loopDemo2():
+    for dawit in range(24,31):
+        print(dawit)
+
+#loopDemo2()
+
+
+def loopDemo3():
+    for freddie in [10,20,30,45] :
+        print(freddie)
+loopDemo3()
