71cc6a62255db2e4e73ea8b970be14bc300ec43a
ndas950 <45467927+ndas950@users.noreply.github.com>
2018-12-06 19:52:42 -0500

    Add files via upload

---
+++ 12_6_2018 classwork.py
@@ -0,0 +1,13 @@
+#12/6/2018 classwork
+
+
+
+
+def classwork():
+
+    print("This is cool","because....",sep=',')
+    print("This is line1\\nthis is line2\nthis is line3\nthat'senough")
+    for n in range(10):
+        print(2*n+1,end=' ') #print no longer goes to a new line after printing, 
+#but adds a space
+classwork()
