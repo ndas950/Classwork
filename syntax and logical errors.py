13839789255b7a443a49c1bb8504fcd0dc2f9437
ndas950 <45467927+ndas950@users.noreply.github.com>
2019-01-13 16:52:10 -0500

    Add files via upload

---
+++ syntax and logical errors.py
@@ -0,0 +1,14 @@
+
+
+# A simple program illustrating chaotic behavior
+
+
+def main():
+    print("This program prints 10 numbers of a chaotic function)
+    x=eval(input("Enter a number between 0 and 1:")
+    for i in range(11):
+        x=3.9*x(1-x)
+    print(x)
+main
+           
+    
